a81d0bf74036b96c292a43e7e7777f17f138cb8e
Levi Salazar <lsalazar-2026039@kinal.edu.gt>
2026-06-09 16:25:29 -0600

    000 Primer commit se agrego la clase connection

---
+++ src/connection/Connection.java
@@ -0,0 +1,13 @@
+
+package connection;
+/**
+ *
+ * @author Levi Salazar Muñoz
+ */
+public class Connection {
+
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+    
+}
